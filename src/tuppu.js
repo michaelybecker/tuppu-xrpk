012cf51c1cf1327687addce3da1e8acaf69e79c6
Michael Hazani <123mickey@gmail.com>
2021-04-14 16:54:29 +0300

    networked reset position

--- src/tuppu.js
+++ src/tuppu.js
@@ -21,7 +21,7 @@ import {
 const clearSans = require("./fonts/ClearSans/ClearSans-Regular.ttf");
 const CURSOR_SPEED_MS = 500;
 
-let isOwner;
+let isOwner = false;
 State.currentText = "";
 State.debugMode = false;
 
@@ -33,10 +33,6 @@ setTimeout(() => {
   Camera.add(frontAnchor);
   Camera.position.z += 1;
 }, 300);
-
-window.addEventListener("mousedown", e => {
-  e.preventDefault();
-});
 
 const UTIL_KEYS = [
   "Shift",
@@ -95,122 +91,6 @@ const GradientMaterial = new ShaderMaterial({
 `,
 });
 
-const light = new DirectionalLight(0xffffff, 3.5);
-light.position.set(0, 13, 3);
-scene.add(light);
-
-const TextBox = new Text();
-scene.add(TextBox);
-
-// Set properties to configure:
-const introText = `
-tuppu: a simple, networked text editor
---------------------------------------
-- Use your keyboard to enter text
-- press both triggers to minimize / restore 
-- Ctrl+X to CUT all text
-- Ctrl+C to COPY all text
-- Ctrl+V to PASTE from clipboard
-`;
-TextBox.text = introText;
-TextBox.font = clearSans;
-TextBox.fontSize = 0.035;
-TextBox.position.z = -0.25;
-TextBox.text = introText;
-
-// if gradient, color and outlinecolor don't take effect
-TextBox.material = GradientMaterial;
-
-TextBox.color = 0xffffff;
-// TextBox.outlineColor = 0xc825fa;
-TextBox.outlineColor = 0x8925fa;
-TextBox.outlineBlur = "5%";
-
-TextBox.maxWidth = 1;
-TextBox.sync();
-
-let cursorVisible = true;
-// there *has* to be a less dumb blinking cursor implementation
-setInterval(() => {
-  if (cursorVisible == true) {
-    TextBox.text += "|";
-  } else {
-    if (TextBox.text[TextBox.text.length - 1] == "|") {
-      TextBox.text = TextBox.text.substring(0, TextBox.text.length - 1);
-    }
-  }
-
-  cursorVisible = !cursorVisible;
-}, CURSOR_SPEED_MS);
-
-const ResetTextPos = () => {
-  const tempCamVec = new Vector3();
-  const tempFrontVec = new Vector3();
-  const tempQuat = new Quaternion();
-  const tempScale = new Vector3();
-  const camProxy = new Object3D();
-
-  frontAnchor.matrixWorld.decompose(tempFrontVec, tempQuat, tempScale);
-  TextBox.position.copy(tempFrontVec);
-  Camera.matrixWorld.decompose(tempCamVec, tempQuat, tempScale);
-  camProxy.position.copy(tempCamVec);
-  TextBox.lookAt(camProxy.position);
-};
-
-const UpdateText = e => {
-  return new Promise(resolve => {
-    switch (e.keyCode) {
-      case 8: // backspace
-        if (State.currentText.length == 0) break;
-        State.currentText = State.currentText.slice(0, -1);
-        resolve();
-        break;
-
-      case 13: // return
-        State.currentText += "\n";
-        TextBox.anchorY = "70%";
-        resolve();
-        break;
-
-      case 17: // ctrl
-        resolve();
-        State.ctrlDown = true;
-
-      default:
-        // A-Z, nums, etc
-        if (UTIL_KEYS.includes(e.key)) {
-          resolve();
-          return;
-        } else {
-          if (State.ctrlDown) {
-            if (e.key == "c") {
-              navigator.clipboard
-                .writeText(State.currentText)
-                .then(console.warn("tuppu: copied text"));
-            } else if (e.key == "x") {
-              navigator.clipboard.writeText(State.currentText);
-              navigator.clipboard.readText().then(clipText => {
-                State.currentText = "";
-                console.warn("tuppu: cut text");
-                resolve();
-              });
-            } else if (e.key == "v") {
-              navigator.clipboard.readText().then(clipText => {
-                State.currentText += clipText;
-                console.warn("tuppu: paste complete");
-                resolve();
-              });
-            }
-          } else {
-            State.currentText += e.key;
-            resolve();
-          }
-          break;
-        }
-    }
-  });
-};
-
 State.eventHandler.addEventListener("selectstart", e => {
   if (!isOwner) return;
 
@@ -234,11 +114,17 @@ class TuppuModel extends Croquet.Model {
   init() {
     this.subscribe("tuppomodel", "update-text-model", this.updateText);
     this.subscribe("tuppomodel", "set-ownerid", this.setOwnerID);
+    this.subscribe("tuppomodel", "reposition", this.reposition);
     this.textString = "";
   }
   updateText(newString) {
     this.textString = newString;
     this.publish("tuppoview", "update-text-view", newString);
+  }
+
+  reposition(data) {
+    this.posData = data;
+    this.publish("tuppoview", "reposition", data);
   }
 
   setOwnerID(viewID) {
@@ -257,14 +143,19 @@ class TuppuView extends Croquet.View {
   constructor(model) {
     super(model);
     this.sceneModel = model;
-
-    this.initText();
     this.initOwnerID();
+
+    this.createTextbox();
+    this.syncState();
 
     this.subscribe("tuppoview", "update-text-view", this.handleUpdate);
     this.subscribe("tuppoview", "set-as-owner", this.setAsOwner);
+    this.subscribe("tuppoview", "reposition", this.reposition);
 
     window.addEventListener("keydown", this.asyncUpdateText.bind(this));
+    window.addEventListener("mousedown", e => {
+      e.preventDefault();
+    });
 
     window.addEventListener("keyup", e => {
       if (e.keyCode == 17) {
@@ -274,9 +165,56 @@ class TuppuView extends Croquet.View {
     });
   }
 
+  createTextbox() {
+    this.TextBox = new Text();
+    scene.add(this.TextBox);
+
+    // Set properties to configure:
+    this.introText = `
+    tuppu: a simple, networked text editor
+    --------------------------------------
+    - Use your keyboard to enter text
+    - press both triggers to minimize / restore 
+    - Ctrl+X to CUT all text
+    - Ctrl+C to COPY all text
+    - Ctrl+V to PASTE from clipboard
+    `;
+    this.TextBox.text = this.introText;
+    this.TextBox.font = clearSans;
+    this.TextBox.fontSize = 0.035;
+    this.TextBox.position.z = -0.25;
+
+    // if gradient, color and outlinecolor don't take effect
+    this.TextBox.material = GradientMaterial;
+
+    this.TextBox.color = 0xffffff;
+    // this.TextBox.outlineColor = 0xc825fa;
+    this.TextBox.outlineColor = 0x8925fa;
+    this.TextBox.outlineBlur = "5%";
+
+    this.TextBox.maxWidth = 1;
+    this.TextBox.sync();
+
+    let cursorVisible = true;
+    // there *has* to be a less dumb blinking cursor implementation
+    setInterval(() => {
+      if (cursorVisible == true) {
+        this.TextBox.text += "|";
+      } else {
+        if (this.TextBox.text[this.TextBox.text.length - 1] == "|") {
+          this.TextBox.text = this.TextBox.text.substring(
+            0,
+            this.TextBox.text.length - 1
+          );
+        }
+      }
+
+      cursorVisible = !cursorVisible;
+    }, CURSOR_SPEED_MS);
+  }
   handleUpdate(newString) {
-    TextBox.text = newString == "" ? introText : newString;
-    TextBox.sync();
+    this.TextBox.text = newString == "" ? this.introText : newString;
+    this.TextBox.sync();
   }
 
   setAsOwner() {
@@ -284,29 +222,115 @@ class TuppuView extends Croquet.View {
     isOwner = true;
   }
 
-  async asyncUpdateText(e) {
-    if (!isOwner) return; // only owner can write
+  reposition(data) {
+    const tempCamVec = new Vector3().fromArray(data.tempCamVecArr);
+    const tempFrontVec = new Vector3().fromArray(data.tempFrontVecArr);
 
-    await UpdateText(e);
+    this.camProxy.position.copy(tempCamVec);
+    this.TextBox.position.copy(tempFrontVec);
+    this.TextBox.lookAt(this.camProxy.position);
+  }
+  async asyncUpdateText(e) {
+    // if (!isOwner) return; // only owner can write
+    await this.handleLocalInput(e);
     this.publish("tuppomodel", "update-text-model", State.currentText);
   }
 
   initOwnerID() {
+    console.log(`tuppu: your ownerID: ${this.viewId}`);
     this.publish("tuppomodel", "set-ownerid", this.viewId);
   }
-  initText() {
+  syncState() {
     if (this.sceneModel.textString != "") {
       this.handleUpdate(this.sceneModel.textString);
       State.currentText = this.sceneModel.textString;
     }
+
+    if (this.sceneModel.posData)
+      this.publish("tuppoview", "reposition", this.sceneModel.posData);
+  }
+
+  handleLocalInput(e) {
+    return new Promise(resolve => {
+      switch (e.keyCode) {
+        case 8: // backspace
+          if (State.currentText.length == 0) break;
+          State.currentText = State.currentText.slice(0, -1);
+          resolve();
+          break;
+
+        case 13: // enter
+          State.currentText += "\n";
+          this.TextBox.anchorY = "70%";
+          resolve();
+          break;
+
+        case 17: // ctrl
+          State.ctrlDown = true;
+          resolve();
+
+        default:
+          // A-Z, nums, etc
+          if (UTIL_KEYS.includes(e.key)) {
+            resolve();
+            return;
+          } else {
+            if (State.ctrlDown) {
+              if (e.key == "c") {
+                navigator.clipboard.writeText(State.currentText).then(e => {
+                  console.warn("tuppu: copied text");
+                  resolve();
+                });
+              } else if (e.key == "x") {
+                navigator.clipboard.writeText(State.currentText);
+                navigator.clipboard.readText().then(clipText => {
+                  State.currentText = "";
+                  console.warn("tuppu: cut text");
+                  resolve();
+                });
+              } else if (e.key == "v") {
+                navigator.clipboard.readText().then(clipText => {
+                  State.currentText += clipText;
+                  console.warn("tuppu: pasted clipboard");
+                  resolve();
+                });
+              }
+            } else {
+              State.currentText += e.key;
+              resolve();
+            }
+            break;
+          }
+      }
+    });
+  }
+
+  ResetTextPos() {
+    const tempCamVec = new Vector3();
+    const tempFrontVec = new Vector3();
+    const tempQuat = new Quaternion();
+    const tempScale = new Vector3();
+    this.camProxy = new Object3D();
+
+    frontAnchor.matrixWorld.decompose(tempFrontVec, tempQuat, tempScale);
+    Camera.matrixWorld.decompose(tempCamVec, tempQuat, tempScale);
+    // camProxy.position.copy(tempCamVec);
+    // this.TextBox.position.copy(tempFrontVec);
+    // this.TextBox.lookAt(camProxy.position);
+    const tempCamVecArr = tempCamVec.toArray();
+    const tempFrontVecArr = tempFrontVec.toArray();
+    const data = { tempCamVecArr, tempFrontVecArr };
+    this.publish("tuppomodel", "reposition", data);
   }
 }
 
 const pmaEventHandler = new PMAEventHandler();
 const xrpkAppId = pmaEventHandler.getAppState().appId;
 const name = xrpkAppId ? xrpkAppId : "tupputuppuwritemynameyes";
+console.log(`name is: ${name}`);
+
 Croquet.Session.join({
-  appId: "com.plutovr.tuppu12",
+  appId: "com.plutovr.tuppu2",
   name: name,
   password: "secret",
   model: TuppuModel,
